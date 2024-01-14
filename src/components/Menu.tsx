2d28442aab5ad2cb8f995e6fa45919358076f5a1
MiguelARey <mianare.99.anaya@gmail.com>
2024-01-14 10:29:36 -0800

    menu html done

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,10 +2,18 @@ function Menu(){
     return(
         <nav>
             <div className="logo">
-
+                <img src="https://media.istockphoto.com/id/1235662202/vector/car-automobile-parking-sign-icon-with-circle-shape.jpg?s=612x612&w=0&k=20&c=aCyUQ4jmvTUfYN4fwcC_1KcYyQOsIdRotOA6FcifJcg=" alt="logo-1" />
+                <h1>PARKAR SA</h1>
             </div>
             <div className="menu">
-
+                <ul>
+                    <li><a href="#">LINK 1</a></li>
+                    <li><a href="#">LINK 1</a></li>
+                    <li><a href="#">LINK 1</a></li>
+                    <li><a href="#">LINK 1</a></li>
+                    <li><a href="#">LINK 1</a></li>
+                    <li><a href="#">LINK 1</a></li>
+                </ul>
             </div>
         </nav>
     )
