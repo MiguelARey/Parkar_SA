fe65cbd1e88905dc09632cc1fe50eadfed2c165d
MiguelARey <mianare.99.anaya@gmail.com>
2024-01-14 10:27:45 -0800

    nav html done

---
+++ src/components/MainPage.tsx
@@ -0,0 +1,13 @@
+import Footer from "./Footer"
+import Nav from "./Nav"
+
+function MainPage(){
+    return(
+        <div>
+            <Nav/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default MainPage
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,17 +1,60 @@
-function Nav(){
-    return(
-        <nav>
-            <div className="corp">
+function Nav() {
+  return (
+    <nav>
+      <div className="corp">
+        <article>
+          <h2>Corporate</h2>
+          <h2>Offices</h2>
+        </article>
+        <div className="dropdown">
+          <a
+            className="btn btn-secondary dropdown-toggle"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Dropdown link
+          </a>
 
-            </div>
-            <div className="schedule">
-
-            </div>
-            <div className="socialm">
-                
-            </div>
-        </nav>
-    )
+          <ul className="dropdown-menu">
+            <li>
+              <aside>Monday</aside>
+              <p>07:00 AM - 07:00 PM</p>
+            </li>
+            <li>
+              <aside>Tuesday</aside>
+              <p>07:00 AM - 07:00 PM</p>
+            </li>
+            <li>
+              <aside>Wednesday</aside>
+              <p>07:00 AM - 07:00 PM</p>
+            </li>
+            <li>
+              <aside>Thursday</aside>
+              <p>07:00 AM - 07:00 PM</p>
+            </li>
+            <li>
+              <aside>Friday</aside>
+              <p>07:00 AM - 07:00 PM</p>
+            </li>
+            <li>
+              <aside>Saturday</aside>
+              <p>07:00 AM - 07:00 PM</p>
+            </li>
+            <li>
+              <aside>Sunday</aside>
+              <p>CLOSED</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="socialm">
+        <i className="fa-brands fa-instagram"></i>
+        <i className="fa-brands fa-facebook"></i>
+      </div>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
