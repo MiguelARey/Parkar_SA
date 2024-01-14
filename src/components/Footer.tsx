76031ff428c0bd38dcc48599fd2d8ba71e055995
MiguelARey <mianare.99.anaya@gmail.com>
2024-01-14 10:12:00 -0800

    footer html complete

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,77 @@
 function Footer (){
     return(
         <div>
-
+            <div className="contact">
+                <div className="icon">
+                    <figure>
+                        <img src="https://media.istockphoto.com/id/1235662202/vector/car-automobile-parking-sign-icon-with-circle-shape.jpg?s=612x612&w=0&k=20&c=aCyUQ4jmvTUfYN4fwcC_1KcYyQOsIdRotOA6FcifJcg=" alt="logo-f" />
+                    </figure>
+                    <figcaption>
+                        <i className="fa-brands fa-instagram"></i>
+                        <i className="fa-brands fa-facebook"></i>
+                    </figcaption>
+                </div>
+                <div className="fmenu">
+                    <h3>
+                        Parkar SA
+                    </h3>
+                    <a href="#">link 1</a>
+                    <a href="#">link 1</a>
+                    <a href="#">link 1</a>
+                    <a href="#">link 1</a>
+                    <a href="#">link 1</a>
+                    <a href="#">link 1</a>
+                    <a href="#">link 1</a>
+                    <a href="#">link 1</a>
+                </div>
+                <div className="links">
+                    <h3>Header 2</h3>
+                    <a href="#">link 2</a>
+                    <a href="#">link 2</a>
+                    <a href="#">link 2</a>
+                </div>
+                <div className="fschedule">
+                    <h3>Parkar Schedule</h3>
+                    <article>
+                        <aside>Monday</aside>
+                        <p>07:00 AM - 07:00 PM</p>
+                    </article>
+                    <article>
+                        <aside>Tueday</aside>
+                        <p>07:00 AM - 07:00 PM</p>
+                    </article>
+                    <article>
+                        <aside>Wednesday</aside>
+                        <p>07:00 AM - 07:00 PM</p>
+                    </article>
+                    <article>
+                        <aside>Thursday</aside>
+                        <p>07:00 AM - 07:00 PM</p>
+                    </article>
+                    <article>
+                        <aside>Friday</aside>
+                        <p>07:00 AM - 07:00 PM</p>
+                    </article>
+                    <article>
+                        <aside>Saturday</aside>
+                        <p>07:00 AM - 07:00 PM</p>
+                    </article>
+                    <article>
+                        <aside>Sunday</aside>
+                        <p>CLOSED</p>
+                    </article>
+                </div>
+            </div>
+            <footer>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, ab?</p>
+                <article>
+                    <a href="#">Privacy Policy</a>
+                    <a href="#">Accesability</a>
+                    <a href="#">Terms of Service</a>
+                    <a href="#">Cookie Preference Center</a>
+                </article>
+            </footer>
         </div>
     )
 }
