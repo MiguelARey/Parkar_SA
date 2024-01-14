5841bbb09132ca1c229db487e6ab4766b4226e1b
MiguelARey <mianare.99.anaya@gmail.com>
2024-01-14 09:50:47 -0800

    setup of components

---
+++ src/components/Menu.tsx
@@ -0,0 +1,14 @@
+function Menu(){
+    return(
+        <nav>
+            <div className="logo">
+
+            </div>
+            <div className="menu">
+
+            </div>
+        </nav>
+    )
+}
+
+export default Menu
---
+++ src/components/Nav.tsx
@@ -0,0 +1,17 @@
+function Nav(){
+    return(
+        <nav>
+            <div className="corp">
+
+            </div>
+            <div className="schedule">
+
+            </div>
+            <div className="socialm">
+                
+            </div>
+        </nav>
+    )
+}
+
+export default Nav
