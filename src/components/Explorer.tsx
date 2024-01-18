7f1033a2a201b8eed899ac3a69d16230cac80562
MiguelARey <mianare.99.anaya@gmail.com>
2024-01-18 10:35:47 -0800

    explorer sectio style progressed, will need to check after getting everything together

--- src/components/Explorer.tsx
+++ src/components/Explorer.tsx
@@ -1,20 +1,94 @@
-function Explorer(){
-    const companies =[
-        {name:'company1',picture:'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png'},{name:'company1',picture:'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png'},{name:'company1',picture:'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png'},{name:'company1',picture:'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png'},{name:'company1',picture:'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png'},{name:'company1',picture:'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png'}
+function Explorer() {
+  const companies = [
+    {
+      name: "company1",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png",
+    },
+    {
+      name: "company1",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png",
+    },
+    {
+      name: "company1",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png",
+    },
+    {
+      name: "company1",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png",
+    },
+    {
+      name: "company1",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png",
+    },
+    {
+      name: "company1",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png",
+    },
+    {
+      name: "company1",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png",
+    },
+    {
+      name: "company1",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png",
+    },
+    {
+      name: "company1",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png",
+    },
+    {
+      name: "company1",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png",
+    },
+    {
+      name: "company1",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png",
+    },
+    {
+      name: "company1",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png",
+    },
+    {
+      name: "company1",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png",
+    },
+    {
+      name: "company1",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Lululemon_Athletica_logo.svg/1200px-Lululemon_Athletica_logo.svg.png",
+    }
+  ];
 
-    ]
-
-    return(
-        <div>
-            <h2>Heading One</h2>
-            <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h3>
-            <section className="gallery">
-                {companies.map(item=><figure>
-                    <img src={item.picture} alt={item.name} />
-                </figure> )}
-            </section>
-        </div>
-    )
+  return (
+    <div className="galCont">
+      <div className="galSection">
+        <aside>
+          <h2>Health, Hospitality and more</h2>
+          <h3>Explore all your solutions at Parking PARKAR</h3>
+        </aside>
+        <section className="gallery">
+          {companies.map((item) => (
+            <figure>
+              <img src={item.picture} alt={item.name} />
+            </figure>
+          ))}
+        </section>
+      </div>
+    </div>
+  );
 }
 
-export default Explorer
+export default Explorer;
