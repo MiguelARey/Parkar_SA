844efb5f2efb8bc4e4673fc9a5acee09bee417b7
MiguelARey <mianare.99.anaya@gmail.com>
2024-01-17 16:38:55 -0800

    carousel is completly functional now, only need data and style

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -1,44 +1,66 @@
-import Carousel from 'react-bootstrap/Carousel';
+import { useState } from "react";
 
-function MainSection() {
+interface Slide {
+  img: string;
+  text1: string;
+  text2: string;
+  title: string;
+}
+
+interface MainSectionProps {
+  slides: Slide[];
+}
+
+function MainSection({ slides }: MainSectionProps) {
+  const [idx, setIdx] = useState(0);
+
+  const prevSlide = () => {
+    const checkFirst = idx === 0;
+    const newIdx = checkFirst ? slides.length - 1 : idx - 1;
+    setIdx(newIdx);
+  };
+
+  const nextSlide = () => {
+    const checkLast = slides.length - 1 === idx;
+    const newIdx = checkLast ? 0 : idx + 1;
+    setIdx(newIdx);
+  };
+
+  const expSlide = (id: number) => {
+    setIdx(id);
+  };
+
   return (
-    <Carousel data-bs-theme="dark">
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://images.pexels.com/photos/18489099/pexels-photo-18489099/free-photo-of-man-in-white-shirt-with-book-in-hands.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://images.pexels.com/photos/10917649/pexels-photo-10917649.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://images.pexels.com/photos/19709063/pexels-photo-19709063/free-photo-of-closeup-of-a-lotus-bud.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h5>Third slide label</h5>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <div className="carousel">
+      <div className="leftArrow" onClick={prevSlide}>
+        &#8592;
+      </div>
+      <div className="rightArrow" onClick={nextSlide}>
+        &#x2192;
+      </div>
+      <div
+        className="slide"
+        style={{ backgroundImage: `url(${slides[idx].img})` }}
+      ></div>
+      <article>
+        <h1>
+        {slides[idx].title}
+        </h1>
+        <h3>
+        {slides[idx].text1}
+        </h3>
+        <h5>
+        {slides[idx].text2}
+        </h5>
+      </article>
+      <div className="carouselExp">
+        {slides.map((item, id) => (
+          <div className="dots" key={id} onClick={()=>expSlide(id)}>
+            &#x2022;
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
